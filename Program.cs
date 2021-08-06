f88f316d3d9d2ef4557b9a6b8cc9f701683b7327
Moises <mfrchg@hotmail.com>
2021-08-06 12:41:53 -0500

    0.0: Create the repo, and general inline to-dos

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tranzact.SearchFight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: 0.0 Create Repo and general To-dos
+            // TODO: 1.0 Find Google, Bing and maybe yahoo keys and add them to the config file.
+            // TODO: 2.0 Create an interface for the clients.
+            // TODO: 3.0 Implement the clients.
+            // TODO: 4.0 Create a Comparer class than implements the clients.
+            // TODO: 4.1 Implement the clients as a list according to the existing in the config file.
+            // TODO: 5.0 Create the UI display methods.
+            // TODO: 6.0 Considerate some DI, in spite of it may be been useless for a console app
+            // TODO: 7.0 Create a parameter parser and parameter type.
+            // TODO: 8.0 Launch the comparer object from the main as an anonymous method.
+            // TODO: 9.0 Check if folder structure makes sense, and refactor if needed.
+        }
+    }
+}
